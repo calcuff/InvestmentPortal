06377159c1dc3824178da5f6b77a105e0d096bfe
calcuff <username>
2020-01-25 15:11:26 -0500

    import router and update pathing

--- investment/src/App.js
+++ investment/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './Components/NavBar'
+import Cart from './Components/Cart'
 import ProductList from './Components/ProductList'
 import Details from './Components/Details'
-import Cart from './Components/Cart'
 import Default from './Components/Default'
 
 
@@ -13,11 +14,13 @@ class App extends Component{
   render() {
     return (
       <React.Fragment>
-        <NavBar></NavBar>
-        <ProductList></ProductList>
-        <Details></Details>
-        <Cart></Cart>
-        <Default></Default>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+          </Switch>  
       </React.Fragment>
     );
   }
