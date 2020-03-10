83664a0c48f0204dbe8fd44fb2c42611dc8df367
calcuff <username>
2020-03-10 16:45:40 -0400

    Implemented auto-complete ticker and quote input form

--- investment/src/Components/Quotes.js
+++ investment/src/Components/Quotes.js
@@ -1,19 +1,163 @@
 import React, { Component } from 'react'
 import Title from './Title'
 import {Link} from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import axios from "axios";
+
 
 export default class Quotes extends Component {
+    state = {
+        symbols: '',
+        query: '',
+        completed: '',
+        isQueryLoading: true,
+        isTickerLoading: true,
+        queried: 0,
+        tickered: 0
+    }
+
+    onChange = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        });
+      }
+
+    onTickerSubmit = async () => {
+        console.log(this.state.symbols);
+        var tickers = this.parseTicker();
+        console.log("Before call, Tickers: ", tickers);
+
+        axios.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols="+tickers, {
+            headers: {
+                'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+                'x-rapidapi-key' : '807803f5d0msh30ebe2386bcb87bp1d620djsnd4f62f748453',
+                'Content-Type': 'application/json' 
+            }
+        }).then(response =>{
+          this.setState({
+            completed: response.data,
+            isTickerLoading: false
+            //tickered: 1
+          });
+          console.log("Got data: ", response.data)
+          //console.log("Symbol1: ", this.state.completed.ResultSet.Result[0].symbol, this.state.completed.ResultSet.Result[0].name ); 
+        })
+        // If we catch any errors connecting, let's update accordingly
+        .catch(error => {
+            console.log("Error: " + error)
+        });
+    
+    }
+
+    onQuerySubmit = async () => {
+        console.log(this.state.query);
+        var company = this.parseQuery();
+        console.log("Before call, Company: ", company);
+
+        axios.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/auto-complete?lang=en&region=US&query="+company, {
+            headers: {
+                'x-rapidapi-host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
+                'x-rapidapi-key' : '807803f5d0msh30ebe2386bcb87bp1d620djsnd4f62f748453',
+                'Content-Type': 'application/json' 
+            }
+        }).then(response =>{
+          this.setState({
+            completed: response.data,
+            isQueryLoading: false,
+            queried: 1
+          });
+          console.log("Got data: ", response.data)
+          console.log("Symbol1: ", this.state.completed.ResultSet.Result[0].symbol, this.state.completed.ResultSet.Result[0].name ); 
+        })
+        // If we catch any errors connecting, let's update accordingly
+        .catch(error => {
+            console.log("Error: " + error)
+        });
+    }
+    parseTicker = () =>{
+        var string = this.state.symbols;
+        string = string.split(',');
+        var tickers = '';
+        for( var i=0; i < string.length; i++){
+            tickers += string[i];
+            if(i != string.length-1){
+                tickers += "%252C";
+            }
+        }
+        return tickers;
+
+    }
+    parseQuery = () => {
+        var string = this.state.query;
+        string = string.split(' ');
+        var company = '';
+        for( var i=0; i < string.length; i++){
+            company += string[i];
+            if(i != string.length-1){
+                company += "%20";
+            }
+        }
+        return company;
+    }
 
     render() {
+        if (this.state.tickered == 0){
         return (
             <React.Fragment>
                 <div className="py-5">
-                <div className="container">
-                    <Title name="Quotes"/>
+                    <div className="container">
+                        <Title name="Quotes"/>
+                    </div>
                 </div>
-                Quotes go here
+                <div style={{display: "flex",justifyContent: "center",}}>
+                    <label >
+                        Enter comma separated stock symbols to get quotes
+                    </label>
                 </div>
+                <div style={{display: "flex",justifyContent: "center",}}>
+                    <input
+                        name='symbols'
+                        placeholder='Tickers'
+                        style={{ width: "300px" }}
+                        onChange={e => this.onChange(e)}
+                        value={this.state.symbols} />
+                </div>
+                <br></br>
+                <div style={{display: "flex",justifyContent: "center",}}>
+                    <Button onClick={() => this.onTickerSubmit()} type="primary">Get Quotes</Button>
+                </div>
+                <br></br>
+                <div style={{display: "flex",justifyContent: "center",}}>
+                    <label >
+                       Search by company name to get auto-complete results for trading symbols
+                    </label>
+                </div>
+                <div style={{display: "flex",justifyContent: "center",}}>
+                    <input
+                        name='query'
+                        placeholder='Company name or symbol'
+                        style={{ width: "300px" }}
+                        onChange={e => this.onChange(e)}
+                        value={this.state.query} />
+                </div>
+                <br></br>
+                <div style={{display: "flex",justifyContent: "center",}}>
+                    <Button variant="secondary" onClick={() => this.onQuerySubmit()} type="primary">Auto-complete</Button>
+                </div>
+
+                { this.state.queried === 1 && this.state.isQueryLoading == false && <div className="container">
+                    <div>Possible companies you were searching for:</div>
+                    {this.state.completed.ResultSet.Result.map((result => 
+                    <div class="card bg-info text-white">
+                    <div class="card-body" key={result.symbol} ></div>
+                    <h6 className="card-title">{result.symbol}</h6>
+                    <h6 className="card-title">{result.name}</h6>
+                    </div>))}
+                    </div>}
+      
+              
             </React.Fragment>
         );
+        }
     }
 }
